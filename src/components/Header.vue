4262a6c09fcc3f1805435bb4f2df6d4b099a5fd6
Livio <livio.brunner.lb1@roche.com>
2017-05-15 09:50:42 +0200

    Make header responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,20 @@
 <template>
-    <header>
-        <section class="row center-xs middle-xs introduction">
-            <div>
-                <div class="row center-xs">
-                    <div class="logo"></div>
-                </div>
-                <h1>
-                    Hi, I'm
-                    <span class="highlight">Livio</span>
-                </h1>
-            </div>
-    
-        </section>
-        <section class="triangles">
-        </section>
-    </header>
+  <header>
+    <section class="row center-xs middle-xs introduction">
+      <div>
+        <div class="row center-xs">
+          <div class="logo"></div>
+        </div>
+        <h1>
+          Hi, I'm
+          <span class="highlight">Livio</span>
+        </h1>
+      </div>
+
+    </section>
+    <section class="triangles">
+    </section>
+  </header>
 </template>
 
 <script>
@@ -24,57 +24,66 @@ export default {
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import '../styles/main';
+
 header {
-    margin: 0;
-    background: #4038a5;
-    background: -moz-radial-gradient(center, ellipse cover, #4038a5 0%, #38248b 100%);
-    background: -webkit-radial-gradient(center, ellipse cover, #4038a5 0%, #38248b 100%);
-    background: radial-gradient(ellipse at center, #4038a5 0%, #38248b 100%);
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#4038a5', endColorstr='#38248b', GradientType=1);
-    width: 100%;
-    height: 80vh;
-    position: relative;
-    padding: 0;
-    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+  margin: 0;
+  background: $primary;
+  @include gradient-radial($primary, $primary_dark);
+  width: 100%;
+  height: 80vh;
+  position: relative;
+  padding: 0;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
 }
 
 h1 {
-    font-family: 'Open Sans', sans-serif;
+  font-family: $header_font;
+  font-size: 6em;
+  color: white;
+  font-weight: normal;
+  margin: 0;
+  @media #{$break-md} {
     font-size: 4em;
-    color: white;
-    font-weight: normal;
-    margin: 0;
+  }
 }
 
 .highlight {
-    background: -webkit-linear-gradient(#FFA5A7, #FF7779);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
+  @include gradient-vertical($accent_light, $accent);
+  color: $accent;
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  text-fill-color: transparent;
 }
 
 .introduction {
-    height: 100%;
-    width: 100%;
-    top: -50px;
-    position:relative;
+  height: 100%;
+  width: 100%;
+  top: -50px;
+  position: relative;
 }
 
 .triangles {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    background: url(../assets/header_triangles.png);
-    background-size: 100% 100%;
-    height: 200px;
-    width: 100%;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  background: url(../assets/header_triangles.png);
+  background-size: 100% 100%;
+  height: 200px;
+  width: 100%;
 }
 
 .logo {
+  background: url(../assets/logo.png);
+  background-size: 100% 100%;
+  position: relative;
+  width: 150px;
+  height: 150px;
+  @media #{$break-md} {
     width: 100px;
     height: 100px;
-    background: url(../assets/logo.png);
-    background-size: 100% 100%;
-    position: relative;
+  }
 }
 </style>
